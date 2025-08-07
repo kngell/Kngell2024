31fa16dae2636292776b6532d7b2376ba349ca28
Kngell <contact@kngell.com>
2025-08-07 15:32:14 +0200

    admin dashboard start

--- .figmaexportrc.js
+++ .figmaexportrc.js
@@ -1,7 +1,7 @@
 const asSass = require("@figma-export/output-styles-as-sass").default;
 
 module.exports = {
-  token: "figd_ZPipZSHndwJS10m_FXzkVcpjud1dsuGL6MOEd4lJ",
+  token: "",
   fileId: "6Oj57EATtfYnNrWHH3x3e6", //"aSskSubvKYW8dBMgznxrSR",
   commands: [
     [
